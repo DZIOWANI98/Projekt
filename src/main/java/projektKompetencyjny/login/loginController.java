167b614641d8e215d5de21c88bf6be868d34b07d
Monika <xdraught@gmail.com>
2019-12-01 15:56:53 +0100

    error message

--- src/main/java/projektKompetencyjny/login/loginController.java
+++ src/main/java/projektKompetencyjny/login/loginController.java
@@ -21,96 +21,96 @@ import java.util.ResourceBundle;
 
 public class loginController implements Initializable {
 
-  @FXML private TextField username_in;
-  @FXML private PasswordField password_in;
-  @FXML private Button login_button;
-  @FXML private Label error_message;
+    @FXML private TextField username_in;
+    @FXML private PasswordField password_in;
+    @FXML private Button login_button;
+    @FXML private Label error_message;
 
-  @Override
-  public void initialize(URL location, ResourceBundle resources) {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
 
-      login_button.setOnMouseClicked(event -> {
+        login_button.setOnMouseClicked(event -> {
 
-      String username = username_in.getText();
-      String password = password_in.getText();
-
-      Uczen uczen = new Uczen();
-
-      ArrayList<Uczen> uczniowe = new ArrayList<>();
-      ArrayList<Nauczyciel> nauczyciele = new ArrayList<>();
-      ArrayList<Rodzic> rodzice = new ArrayList<>();
+            String username = username_in.getText();
+            String password = password_in.getText();
 
 
-      Configuration con = new Configuration().configure().addAnnotatedClass(Uczen.class);
-      SessionFactory sessionFactory = con.buildSessionFactory();
-      //Session session = sessionFactory.openSession();
-      //Transaction transaction = session.beginTransaction();
-
-      Session session = sessionFactory.openSession();
-      Transaction tx = null;
+            ArrayList<Uczen> uczniowe = new ArrayList<>();
+            ArrayList<Nauczyciel> nauczyciele = new ArrayList<>();
+            ArrayList<Rodzic> rodzice = new ArrayList<>();
 
 
+            Configuration con = new Configuration().configure().addAnnotatedClass(Uczen.class);
+            SessionFactory sessionFactory = con.buildSessionFactory();
+            //Session session = sessionFactory.openSession();
+            //Transaction transaction = session.beginTransaction();
+
+            Session session = sessionFactory.openSession();
+            Transaction tx = null;
 
 
+            try {
 
-       try {
-
-         FXMLLoader loader = new FXMLLoader();
-         loader.setLocation(Main.class.getClassLoader().getResource("ui_clientRoot.fxml"));
-         TabPane root = loader.load();
-         clientRootController controller = loader.getController();
-
-
-         tx = session.beginTransaction();
-         List employees = session.createQuery("FROM Uczen").list();
-         for (Iterator iterator1 = employees.iterator(); iterator1.hasNext();){
-           Uczen employee = (Uczen) iterator1.next();
-           uczniowe.add(employee);
-           if(employee.getEmail().equals(username) && employee.getHaslo().equals(password)){
-             controller.setUserStatus("uczen");
-           }
-         }
-         tx.commit();
-
-         tx = session.beginTransaction();
-         List teachers = session.createQuery("FROM Nauczyciel").list();
-         for (Iterator iterator1 = teachers.iterator(); iterator1.hasNext();){
-           Nauczyciel teacher = (Nauczyciel) iterator1.next();
-           nauczyciele.add(teacher);
-           if(teacher.getEmail().equals(username) && teacher.getHaslo().equals(password)){
-             controller.setUserStatus("nauczyciel");
-           }
-         }
-         tx.commit();
+                FXMLLoader loader = new FXMLLoader();
+                loader.setLocation(Main.class.getClassLoader().getResource("ui_clientRoot.fxml"));
+                TabPane root = loader.load();
+                clientRootController controller = loader.getController();
+                controller.setUserStatus("brak");
 
 
-         tx = session.beginTransaction();
-         List parents = session.createQuery("FROM Rodzic").list();
-         for (Iterator iterator1 = parents.iterator(); iterator1.hasNext();){
-           Rodzic parent = (Rodzic) iterator1.next();
-           rodzice.add(parent);
-           if(parent.getEmail().equals(username) && parent.getHaslo().equals(password)){
-             controller.setUserStatus("rodzic");
-           }
-         }
-         tx.commit();
+                tx = session.beginTransaction();
+                List employees = session.createQuery("FROM Uczen").list();
+                for (Iterator iterator1 = employees.iterator(); iterator1.hasNext();){
+                    Uczen employee = (Uczen) iterator1.next();
+                    uczniowe.add(employee);
+                    if(employee.getEmail().equals(username) && employee.getHaslo().equals(password)){
+                        controller.setUserStatus("uczen");
+                    }
+                }
+                tx.commit();
+
+                tx = session.beginTransaction();
+                List teachers = session.createQuery("FROM Nauczyciel").list();
+                for (Iterator iterator1 = teachers.iterator(); iterator1.hasNext();){
+                    Nauczyciel teacher = (Nauczyciel) iterator1.next();
+                    nauczyciele.add(teacher);
+                    if(teacher.getEmail().equals(username) && teacher.getHaslo().equals(password)){
+                        controller.setUserStatus("nauczyciel");
+                    }
+                }
+                tx.commit();
 
 
-          controller.generateUserInterface();
+                tx = session.beginTransaction();
+                List parents = session.createQuery("FROM Rodzic").list();
+                for (Iterator iterator1 = parents.iterator(); iterator1.hasNext();){
+                    Rodzic parent = (Rodzic) iterator1.next();
+                    rodzice.add(parent);
+                    if(parent.getEmail().equals(username) && parent.getHaslo().equals(password)){
+                        controller.setUserStatus("rodzic");
+                    }
+                }
+                tx.commit();
 
 
-          Stage stage = new Stage();
-          stage.setTitle("Main app");
-          stage.setScene(new Scene(root));
-          stage.show();
+                if(controller.getUserStatus().matches("uczen|rodzic|nauczyciel")) {
+                    controller.generateUserInterface();
 
-          Stage window = (Stage) login_button.getScene().getWindow();
-          window.close();
+                    Stage stage = new Stage();
+                    stage.setTitle("Main app");
+                    stage.setScene(new Scene(root));
+                    stage.show();
 
-        }
-        catch (Exception e) {
-          e.printStackTrace();
-        }
-    });
-  }
+                    Stage window = (Stage) login_button.getScene().getWindow();
+                    window.close();
+                } else {
+                    error_message.setText("Niepoprawne dane.");
+                }
+
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
 }
